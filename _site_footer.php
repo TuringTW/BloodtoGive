4b0f5369ddfc3518bf1d7b407d25f0a7c578677d
TuringTW <kevin831130810620@gmail.com>
2015-07-03 14:10:36 +0800

    Bloodtogive azure version

---
+++ _site_footer.php
@@ -0,0 +1,15 @@
+        			<br>
+        			<br>
+        			<hr>
+        			<footer>
+                <p>&copy; BloodtoGive 2015</p>
+            </footer>
+        </div> <!-- /container -->
+    </body>
+
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<?php if (function_exists('js_section')): 
+	js_section();
+endif ?>
+    
+</html>
