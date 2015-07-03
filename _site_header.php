13286543b21373d7ae4095b15bbdf075062dd6ee
TuringTW <kevin831130810620@gmail.com>
2015-07-03 20:44:13 +0800

    add donate tip

--- _site_header.php
+++ _site_header.php
@@ -62,7 +62,7 @@ require_once('mysql_info.php'); ?>
                         <li <?=($sidebar==2)?'class="active"':''?>><a href="About.php">About</a></li>
                         <li <?=($sidebar==3)?'class="active"':''?>><a href="findsite.php">即刻捐血</a></li>
                         <!-- <li <?=($sidebar==4)?'class="active"':''?>><a href="diary.php">捐血日誌</a></li> -->
-                        <!-- <li <?=($sidebar==5)?'class="active"':''?>><a href="#tip" onclick="alert('建置中XD')">捐血常識</a></li>                         -->
+                        <li <?=($sidebar==5)?'class="active"':''?>><a href="http://www.blood.org.tw/Internet/main/blood_before/index.html">捐血須知</a></li>                        
                         <li <?=($sidebar==6)?'class="active"':''?>><a href="https://github.com/turingtw/bloodtogive">Github</a></li>
                         <li <?=($sidebar==7)?'class="active"':''?>><a href="https://ossonazure.bhuntr.com/index.php">雲端防災大賽</a></li>
                     </ul>
